165ea0795e7809099541b28e29433b42580a64b6
Nathan Vu <nathanvu99@gmail.com>
2020-10-13 19:16:53 -0700

    Working app.js, prompts and generates team.html output

--- Develop/app.js
+++ Develop/app.js
@@ -10,57 +10,138 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Array to hold all employee objects
+const employeeInfo = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-async function init() {
-  const userResponse = await inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: "What is your manager's name?"
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: "What is your manager's id?"
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "What is your manager's email?"
-    },
-    {
-      type: 'input',
-      name: 'officeNum',
-      message: "What is your manager's office number?"
-    },
-    {
-      type: 'list',
-      message: 'Which type of team member would you like to add?',
-      choices: ['Engineer', 'Intern', 'Employee', 'No more team members'],
-      name: 'addMember'
+init = () => {
+  // Prompts user for manager information
+  manager = () => {
+    return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is your manager's name?"
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is your manager's id?"
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is your manager's email?"
+      },
+      {
+        type: 'input',
+        name: 'officeNum',
+        message: "What is your manager's office number?"
+      }
+    ]).then(function(response){
+      // Creates manager object based on user response
+      const manager = new Manager(response.name, response.id, response.email, response.officeNum);
+      employeeInfo.push(manager);
+      addMember();
+    })
+  }
+
+  // Prompts user to add more members or not
+  addMember = () => {
+    return inquirer
+    .prompt ([
+      {
+        type: 'list',
+        message: 'Which type of team member would you like to add?',
+        choices: ['Engineer', 'Intern', 'No more team members'],
+        name: 'addMember'
+      }
+    ]).then(function(response) {
+      switch(response.addMember) {
+        case 'Engineer':
+          engineer();
+          break;
+        case 'Intern':
+          intern();
+          break;
+        default:
+          createTeam();
+      }
+    })
+  }
+
+  // Prompt and create engineer object
+  engineer = () => {
+    return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is your engineer's name?"
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is your engineer's id?"
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is your engineer's email?"
+      },
+      {
+        type: 'input',
+        name: 'githubUser',
+        message: "What is your engineer's Github username?"
+      }
+    ]).then(function(response) {
+      // Creates engineer object based on user response
+      const engineer = new Engineer(response.name, response.id, response.email, response.githubUser);
+      employeeInfo.push(engineer);
+      addMember();
+    })
+  }
+
+  // Prompt and create intern object
+  intern = () => {
+    return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is your intern's name?"
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is your intern's id?"
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is your intern's email?"
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: "What school does your intern come from?"
+      }
+    ]).then(function(response){
+      // Creates intern object based on user response
+      const intern = new Intern(response.name, response.id, response.email, response.school);
+      employeeInfo.push(intern);
+      addMember();
+    });
+  }
+
+  // Creating team and calling render function with employeeInfo array
+  createTeam = () => {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
     }
-  ]);
+    fs.writeFileSync(outputPath, render(employeeInfo), 'utf-8');
+}
+  manager();
 }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+init();
